ee9e95170d9dfac942d710a8cbce5f8d88b09a91
massone99 <lorenzomassone@gmail.com>
2023-03-27 15:19:29 +0200

    Implemented a simple Barrier

--- app/src/main/java/pcd/lab04/monitors/barrier/FakeBarrier.java
+++ app/src/main/java/pcd/lab04/monitors/barrier/FakeBarrier.java
@@ -5,21 +5,20 @@ package pcd.lab04.monitors.barrier;
  */
 public class FakeBarrier implements Barrier {
 
-	private int numThreads;
+    private int numThreads;
 
-	public FakeBarrier(int numThreads) {
-		this.numThreads = numThreads;
-	}
-	
-	@Override
-	public synchronized void hitAndWaitAll()  throws InterruptedException {
-		numThreads--;
-		if (numThreads > 0) {
-			wait();
-		} else {
-			notifyAll();
-		}
-	}
+    public FakeBarrier(int numThreads) {
+        this.numThreads = numThreads;
+    }
 
-	
+    @Override
+    public synchronized void hitAndWaitAll() throws InterruptedException {
+        numThreads--;
+        while (numThreads > 0) {
+            wait();
+        }
+        notifyAll();
+    }
+
+
 }
